f81bffb042744b4e15fd64d90990ae05e4c2fbbe
BiLLie WiLLie <vadimbelinovich@gmail.com>
2024-02-25 21:23:23 +0300

    initial

---
+++ src/components/TheLogo.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { PAGE_TIMELINE } from '../constants'
+</script>
+
+<template>
+  <a :href="`#${PAGE_TIMELINE}`">
+    <img
+      src="@/assets/logo.svg"
+      alt="Logo"
+      class="h-9" />
+  </a>
+</template>
